841c940e3ff4669f9a2397ed344ff4dadde444f0
jabreu92 <jose.abreu4@hotmail.com>
2021-03-15 23:41:43 -0700

    large card display

---
+++ components/LargeCardDisplay.vue
@@ -0,0 +1,34 @@
+<template>
+<div>
+<b-container fluid >
+     <h4 class="header">
+            Events Near You
+        </h4>
+        <p class="snippet" >
+            Events include different genres !
+        </p>
+        <b-container fluid class="cards-container">
+            
+        </b-container>
+</b-container>
+
+</div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .header {
+        font-weight: 700;
+        font-size: 1.5rem;
+    }
+    .snippet {
+        color: grey;
+        margin-bottom: 1.5rem;
+    }
+</style>
