eff4f77328f0c4146c24ee31ee8a158a7b05ae68
jabreu92 <jose.abreu4@hotmail.com>
2021-03-17 15:33:40 -0700

    axios setup

---
+++ components/Events.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-container fluid>
+      <h3>Trending Music Events in Seattle</h3>
+      <div>
+        {{events}}
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+        data(){
+            return {
+                events: null,
+                errored: false
+            }
+        },
+
+    mounted () {
+     axios
+        .get('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=QLnzwCGhWDMWq3z894HvbEL1QuKH2XGw')
+        .then(response => (this.events = response.data))
+        .catch(error => {
+            console.log(error)
+            this.errored = true
+            })
+        .finally(() => this.loading = false)
+        
+        }
+
+    }
+
+</script>
+
+<style scoped></style>
--- components/LargeCardDisplay.vue
+++ components/LargeCardDisplay.vue
@@ -1,15 +1,8 @@
 <template>
 <div>
 <b-container fluid >
-     <h4 class="header">
-            Events Near You
-        </h4>
-        <p class="snippet" >
-            Events include different genres !
-        </p>
-        <b-container fluid class="cards-container">
-            
-        </b-container>
+    <Events />
+       
 </b-container>
 
 </div>
@@ -23,12 +16,5 @@ export default {
 </script>
 
 <style scoped>
-    .header {
-        font-weight: 700;
-        font-size: 1.5rem;
-    }
-    .snippet {
-        color: grey;
-        margin-bottom: 1.5rem;
-    }
+  
 </style>
