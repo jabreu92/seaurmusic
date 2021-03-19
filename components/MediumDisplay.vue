96abacd4cdc4fe75fe1bca0b4be092eb7b71d13c
jabreu92 <jose.abreu4@hotmail.com>
2021-03-18 21:59:21 -0700

    concept of event categories in prog

---
+++ components/MediumDisplay.vue
@@ -0,0 +1,27 @@
+<template>
+<div>
+    <b-container fluid>
+     <b-row>
+       <b-col lg="12">
+           <DeckCards />
+        </b-col>
+    </b-row>
+    </b-container>   
+</div>
+  
+</template>
+
+<script>
+import DeckCards from './DeckCards.vue'
+
+export default {
+  components: { DeckCards },
+   
+
+}
+</script>
+
+<style scoped>
+
+   
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <DeckCards />
+    <MediumDisplay />
   </div>
 </template>
 
