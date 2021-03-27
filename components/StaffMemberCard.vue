3b2889a630488d846a21eb00cb6383d5cdd05923
jabreu92 <jose.abreu4@hotmail.com>
2021-03-27 13:32:51 -0700

    testing beta

--- components/StaffMemberCard.vue
+++ components/StaffMemberCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-     <b-card bg-variant="secondary" text-variant="white"  :title="staff.name.first + staff.name.last" class="mt-1 mb-1 ">
-      <BootstrapImage :image-src-url="staff.picture.large" /> 
-      <b-card-text> Email : {{staff.email}} </b-card-text>
-      <b-card-text> Phone: {{staff.phone}} </b-card-text>
-      <b-card-text> Hometown: {{staff.location.country}} </b-card-text>
-      <b-card-text> Age: {{staff.dob.age}} </b-card-text>
+     <b-card bg-variant="secondary" text-variant="white"  :title="staffFullName" class="mt-1 mb-1 ">
+      <BootstrapImage :image-src-url="staffPic" /> 
+      <b-card-text> Email : {{staffEmail}} </b-card-text>
+      <b-card-text> Phone: {{staffPhone}} </b-card-text>
+      <b-card-text> Hometown: {{staffHometown}} </b-card-text>
+      <b-card-text> Age: {{staffAge}} </b-card-text>
       <template #footer>
         <small >Last Updated 3min ago</small>
       </template>
